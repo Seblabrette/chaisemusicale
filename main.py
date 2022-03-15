da3ecfa54c2749a9d44f9423509633092f646802
Sebastien Pouvreau <sebastien.pouvreau@gmail.com>
2022-03-15 15:35:58 +0100

    modifs en cours de dév

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivy.uix.dropdown import DropDown
 
 from sound_service import SoundService
 from pygame import mixer
+import time
 
 mixer.init()
 
@@ -14,6 +15,7 @@ class MainWidget(BoxLayout):
     selected_song_tag = StringProperty("choisir un titre")
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.is_paused = False
         self.soundservice = SoundService()
         self.nb_songs = self.soundservice.get_nb_songs()
         print("nombre de chansons: " + str(self.nb_songs))
@@ -37,6 +39,7 @@ class MainWidget(BoxLayout):
     def select_song(self, widget):
         self.dropdown.select(widget.text)
         self.selected_song_tag = widget.text
+        # Lecture du son sélectionné
         # for i in range(self.nb_songs):
         #     if widget.text == self.soundservice.songlist.songs[i].tag:
         #         self.soundservice.playsong(i, mixer)
@@ -44,7 +47,44 @@ class MainWidget(BoxLayout):
     def on_parent(self, widget, parent):
         pass
 
+    def press_play(self):
+        if self.selected_song_tag == "choisir un titre":
+            print("Pas de chanson sélectionnée")
+            return None
+        print("PLAY")
+        # Lecture du son sélectionné
+        for i in range(self.nb_songs):
+            if self.selected_song_tag == self.soundservice.songlist.songs[i].tag:
+                self.soundservice.playsong(i, mixer)
+                self.is_paused = False
+
+    def press_pause_resume(self):
+        if self.selected_song_tag == "choisir un titre":
+            print("Pas de chanson sélectionnée")
+            return None
+        if self.is_paused:
+            mixer.music.unpause()
+            print("RESUME")
+            self.is_paused = False
+        else:
+            mixer.music.pause()
+            print("PAUSE")
+            self.is_paused = True
+
+
+    def press_stop(self):
+        if self.selected_song_tag == "choisir un titre":
+            print("Pas de chanson sélectionnée")
+            return None
+        print("STOP")
+        # Arrêt du son en cours de lecture
+        mixer.music.stop()
+
+
+    print("mixer" + str(mixer.get_init()))
+
 class MonAppliApp(App):
     pass
+
 
 MonAppliApp().run()
